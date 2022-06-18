f73f487770469f57e4ffa90d662ea26b1ee916d8
AlexandrAlekseev <alexandrshuriken@gmail.com>
2022-06-18 19:51:40 +0300

    третья др

---
+++ Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да   */
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите пятизначное число : ");
+string number = ReadLine();
+
+
+try//только ли числа в введённом значении?
+{
+    int truNumber = Convert.ToInt32(number);
+    if(Math.Abs(truNumber)<10000 || Math.Abs(truNumber)>=100000)//проверка на кол-во символов
+    {
+        WriteLine("Это не пятизначное число");    
+    }
+    else if(truNumber>0)
+    {
+        WriteLine((number[0] == number[4] && number[1] == number[3])?($"{number} -> Да,полиндром!") : $"{number} -> Не полиндром!");    
+    }
+    else//для отрицательных чисел
+    {
+        WriteLine((number[1] == number[5] && number[2] == number[4])?($"{number} -> Да,полиндром!") : $"{number} -> Не полиндром!");
+    }
+
+}  
+catch
+{
+    WriteLine("Это не число");
+}  
+
+
+
---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+
+cubeTable(N);
+
+int cubeTable(int c)
+{
+    int cubeNumber = 0;
+    for(int i = 1; i<c ; i++)
+    {
+        Write($"{Math.Pow(i ,3)}, ");
+
+    }
+    Write($"{Math.Pow(c ,3)}.\n");
+    return cubeNumber;
+}
